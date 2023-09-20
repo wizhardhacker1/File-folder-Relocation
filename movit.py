77eef9aec87c7b3b0c9fe5822a6cf68e14dfeeda
DAS <46620390+wizhardhacker1@users.noreply.github.com>
2023-09-19 19:00:29 -0700

    Update movit.py

--- movit.py
+++ movit.py
@@ -5,23 +5,23 @@ from tkinter import ttk
 import os
 import threading
 
-def move_with_progress(source, destination, progress_bar):
+def move_with_progress(sources, destination, progress_bar):
     try:
-        shutil.move(source, destination)
+        for source in sources:
+            shutil.move(source, destination)
         status_label.config(text="Move completed.")
         progress_bar.stop()
         move_button.config(state=tk.NORMAL)
         browse_source_button.config(state=tk.NORMAL)
         browse_destination_button.config(state=tk.NORMAL)
-        source_entry.delete(0, tk.END)
+        source_listbox.delete(0, tk.END)
         destination_entry.delete(0, tk.END)
     except Exception as e:
         status_label.config(text=f"Move failed: {str(e)}")
 
 def browse_source_directory():
     source_directory = filedialog.askdirectory()
-    source_entry.delete(0, tk.END)
-    source_entry.insert(0, source_directory)
+    source_listbox.insert(tk.END, source_directory)
 
 def browse_destination_directory():
     destination_directory = filedialog.askdirectory()
@@ -29,9 +29,9 @@ def browse_destination_directory():
     destination_entry.insert(0, destination_directory)
 
 def move_button_clicked():
-    source = source_entry.get()
+    sources = source_listbox.get(0, tk.END)
     destination = destination_entry.get()
-    if source and destination:
+    if sources and destination:
         move_button.config(state=tk.DISABLED)
         browse_source_button.config(state=tk.DISABLED)
         browse_destination_button.config(state=tk.DISABLED)
@@ -42,13 +42,13 @@ def move_button_clicked():
         progress_bar.start()
 
         # Use a separate thread for the move operation to avoid freezing the GUI
-        move_thread = threading.Thread(target=move_with_progress, args=(source, destination, progress_bar))
+        move_thread = threading.Thread(target=move_with_progress, args=(sources, destination, progress_bar))
         move_thread.start()
     else:
-        status_label.config(text="Please specify both source and destination directories!")
+        status_label.config(text="Please specify source and destination directories!")
 
 def reset_button_clicked():
-    source_entry.delete(0, tk.END)
+    source_listbox.delete(0, tk.END)
     destination_entry.delete(0, tk.END)
     status_label.config(text="")
     move_button.config(state=tk.NORMAL)
@@ -61,11 +61,11 @@ root.title("Folder Move GUI")
 frame = tk.Frame(root)
 frame.pack(padx=20, pady=20)
 
-source_label = tk.Label(frame, text="Source Directory:")
+source_label = tk.Label(frame, text="Source Directories:")
 source_label.pack()
 
-source_entry = tk.Entry(frame, width=50)
-source_entry.pack()
+source_listbox = tk.Listbox(frame, selectmode=tk.MULTIPLE, width=50)
+source_listbox.pack()
 
 browse_source_button = tk.Button(frame, text="Browse", command=browse_source_directory)
 browse_source_button.pack()
